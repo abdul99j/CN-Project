9b59f577954dd02486311232bfbe86d6cbd4c49a
abdul99j <abdul.dps@gmail.com>
2020-06-10 23:18:46 +0500

    SMTP updated

--- src/cnup/SMTPPacket.java
+++ src/cnup/SMTPPacket.java
@@ -5,10 +5,24 @@
  */
 package cnup;
 
+import java.util.ArrayList;
+import jpcap.packet.Packet;
+import jpcap.packet.TCPPacket;
+
 /**
  *
  * @author Lenovo
  */
-public class SMTPPacket {
+public class SMTPPacket extends TCPpacket{
+
+    @Override
+    public Boolean isPacketof(Packet p) {
+        if(p instanceof TCPPacket &&
+		   (((TCPPacket)p).src_port==25 || ((TCPPacket)p).dst_port==25))
+			return true;
+		else return false;
+    }
+
+    
     
 }
